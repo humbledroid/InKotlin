d3577db7707bed957d0a700ed4cb6eae5022b2ec
humbledroid <carotkut12@gmail.com>
2026-01-21 21:16:07 +0530

    neetcode begings

--- src/main/kotlin/neetcode/ContainsDuplicate.kt
+++ src/main/kotlin/neetcode/ContainsDuplicate.kt
@@ -2,13 +2,28 @@ package neetcode
 
 class ContainsDuplicate {
     fun containsDuplicate(nums: IntArray): Boolean {
-        val hashSet = HashSet<Int>()
-        nums.forEach {
-            if(hashSet.contains(it)){
-                return false
-            }
-            hashSet.add(it)
+        // Bruteforce
+//        for (i in nums.indices) {
+//            for (j in i+1 until nums.size) {
+//                if (nums[j] == nums[i]) return true
+//            }
+//        }
+//        return false
+
+    // Quick and witty
+    //return nums.toHashSet().size != nums.size
+
+        // a little performant, as whole set is not needed to be built
+        val seen = mutableSetOf<Int>()
+        for (num in nums) {
+            if (num in seen) return true
+            seen.add(num)
         }
-        return true
+        return false
+
     }
+}
+fun main() {
+    val contains = ContainsDuplicate()
+    println(contains.containsDuplicate(intArrayOf(1, 2, 3)))
 }
