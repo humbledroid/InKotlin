f4289d6899a0038b28aa471f2f957c16927c9af6
humbledroid <carotkut12@gmail.com>
2026-01-23 21:07:59 +0530

    micro improvement

--- src/main/kotlin/neetcode/GroupAnagram.kt
+++ src/main/kotlin/neetcode/GroupAnagram.kt
@@ -1,8 +1,22 @@
 package neetcode
 
 class GroupAnagram {
+
+    /**
+    With sorting and using, sorted item as key
+     */
+    // fun groupAnagrams(strs: Array<String>): List<List<String>> {
+    //     val result = mutableMapOf<String, MutableList<String>>()
+    //     strs.forEach { item ->
+    //         val key = item.toCharArray().sorted().joinToString("")
+    //         result.getOrPut(key) { mutableListOf() }.add(item)
+    //     }
+
+    //     return result.values.toList()
+    // }
+
     fun groupAnagrams(strs: Array<String>): List<List<String>> {
-        val result = mutableMapOf<String, MutableList<String>>()
+        val result = mutableMapOf<Int, MutableList<String>>()
         strs.forEach { item ->
             val key = makeAkey(item)
             result.getOrPut(key) { mutableListOf() }.add(item)
@@ -11,13 +25,21 @@ class GroupAnagram {
         return result.values.toList()
     }
 
-    private fun makeAkey(input: String): String {
+    private fun makeAkey(input: String): Int {
         val keyArray = IntArray(26) {0}
         input.forEach {
             keyArray[it - 'a']++
         }
 
-        return keyArray.joinToString("#")
+
+        //return keyArray.joinToString("#")
+
+        /**
+         * here using int via hashcode as key, is significantly faster
+         * as we don't have to iterate over the list, to make a string
+         * out of it via a delimiter
+         */
+        return keyArray.contentHashCode()
     }
 }
 
