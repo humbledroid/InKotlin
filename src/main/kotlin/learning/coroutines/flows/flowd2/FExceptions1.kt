86a5487f8a986950e95cb4bf4f291818b9a8d3b1
humbledroid <carotkut12@gmail.com>
2026-01-24 15:41:17 +0530

    handling exception with catch, try-catch

---
+++ src/main/kotlin/learning/coroutines/flows/flowd2/FExceptions1.kt
@@ -0,0 +1,78 @@
+package learning.coroutines.flows.flowd2
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.runBlocking
+import kotlin.time.Duration.Companion.milliseconds
+
+fun main() = runBlocking {
+//    //try catch can also come handy
+//    try {
+//        flow {
+//            emit("🥛")
+//            emit("🍉")
+//            emit("🍊")
+//            emit("🍇")
+//            emit("🥦")
+//        }
+//            .onEach {
+//                println("Looking for: $it")
+//            }
+//            //this alone gets the exception and terminates the flows
+////        .transform {
+////            emit(findOnShelf(it))
+////        }
+//            .transform {
+//                /**
+//                 * A local try catch can be added to catch the exception
+//                 * and supply a substitute or handle the exception as deems fit
+//                 */
+////            try {
+////                emit(findOnShelf(it))
+////            }catch (ex: OutOfStockException) {
+////                emit(substitute(it))
+////            }
+//                emit(findOnShelf(it))
+//            }
+//            .collect {
+//                println("Adding to cart: ${it.label}")
+//            }
+//    }catch (e: OutOfStockException) {
+//        println("OutOfStockException: ${e.message}")
+//    }
+
+    flow {
+        emit("🥛")
+        emit("🍉")
+        emit("🍊")
+        emit("🍇")
+        emit("🥦")
+    }
+        .onEach {
+            println("Looking for: $it")
+        }
+        .map { findOnShelf(it) }
+        .catch { throwable ->
+            if(throwable is OutOfStockException) emit(GroceryItem("$5 Coupon", price = -5_00))
+        }
+        .collect {
+            println("Adding to cart: ${it.label}")
+        }
+
+    println("Done with grocery shopping")
+}
+
+suspend fun findOnShelf(label: String): GroceryItem {
+    delay(250.milliseconds)
+    return when (label) {
+        "🥛"  -> GroceryItem("🥛 Milk", 3_39)
+        "🍉"  -> GroceryItem("🍉 Watermelon", 5_99)
+        "🍇"  -> GroceryItem("🍇 Grapes", 5_49)
+        "🥦"  -> GroceryItem("🥦 Broccoli", 2_99)
+        else -> throw OutOfStockException(label)
+    }
+}
+
+data class GroceryItem(val label: String, val price: Int)
+data class OutOfStockException(val label: String) : Exception("$label is out of stock.")
+fun substitute(label: String) = GroceryItem("🍎 Apple", 1_29)
