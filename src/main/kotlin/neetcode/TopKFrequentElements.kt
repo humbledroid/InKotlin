dc8ad662598c539198be1e649ab70ea9c7596ae4
humbledroid <carotkut12@gmail.com>
2026-01-25 19:35:40 +0530

    using priority queue

--- src/main/kotlin/neetcode/TopKFrequentElements.kt
+++ src/main/kotlin/neetcode/TopKFrequentElements.kt
@@ -1,5 +1,7 @@
 package neetcode
 
+import java.util.*
+
 class TopKFrequentElements {
     // Simplest
 //    fun topKFrequent(nums: IntArray, k: Int): IntArray {
@@ -12,30 +14,55 @@ class TopKFrequentElements {
 //    }
 
     // with bucket sort
+//    fun topKFrequent(nums: IntArray, k: Int): IntArray {
+//        val frequencyMap = mutableMapOf<Int, Int>()
+//        nums.forEach {
+//            frequencyMap[it] = frequencyMap.getOrDefault(it, 0) + 1
+//        }
+//
+//        val result = mutableListOf<Int>()
+//
+//        val bucket = MutableList<MutableList<Int>>(nums.size+1){mutableListOf()}
+//
+//        frequencyMap.forEach { (num, count) ->
+//            bucket[count].add(num)
+//        }
+//        // now we have the bucket, or sort of count sort bucket, where
+//        // higher index is the most frequent one
+//
+//        for (i in bucket.size - 1 downTo 0) {
+//            bucket[i].forEach { n ->
+//                result.add(n)
+//                if(result.size == k) return result.toIntArray()
+//            }
+//        }
+//
+//        return result.toIntArray()
+
+
+
     fun topKFrequent(nums: IntArray, k: Int): IntArray {
         val frequencyMap = mutableMapOf<Int, Int>()
         nums.forEach {
             frequencyMap[it] = frequencyMap.getOrDefault(it, 0) + 1
         }
 
-        val result = mutableListOf<Int>()
-
-        val bucket = MutableList<MutableList<Int>>(nums.size+1){mutableListOf()}
+        val priorityQueue = PriorityQueue<Pair<Int, Int>>(
+            compareBy {
+                it.second
+            }
+        )
 
         frequencyMap.forEach { (num, count) ->
-            bucket[count].add(num)
-        }
-        // now we have the bucket, or sort of count sort bucket, where
-        // higher index is the most frequent one
-
-        for (i in bucket.size - 1 downTo 0) {
-            bucket[i].forEach { n ->
-                result.add(n)
-                if(result.size == k) return result.toIntArray()
+            priorityQueue.offer(num to count)
+            if(priorityQueue.size > k) {
+                priorityQueue.poll()
             }
         }
 
-        return result.toIntArray()
+        return IntArray(k) {
+            priorityQueue.poll().first
+        }
     }
 }
 
