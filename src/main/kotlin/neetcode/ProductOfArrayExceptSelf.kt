8479a63ea2e1edda53017d5cfa2a6586040aad86
humbledroid <carotkut12@gmail.com>
2026-01-26 16:37:35 +0530

    no divison, suffix prefix way

--- src/main/kotlin/neetcode/ProductOfArrayExceptSelf.kt
+++ src/main/kotlin/neetcode/ProductOfArrayExceptSelf.kt
@@ -24,29 +24,50 @@ class ProductOfArrayExceptSelf {
 //
 //        return result
 
-        val countOfZero = nums.count { it == 0 }
-        val result = IntArray(nums.size) { 0 }
-        if (countOfZero > 1) return result
+//        val countOfZero = nums.count { it == 0 }
+          // val result = IntArray(nums.size) { 0 }
+//        if (countOfZero > 1) return result
+//
+//        val totalNonZeroProduct = nums.filter { it != 0 }.fold(1) { acc, i -> acc * i }
+//
+//        if (countOfZero == 1) {
+//            for (i in nums.indices) {
+//                if (nums[i] == 0) {
+//                    result[i] = totalNonZeroProduct
+//                } else {
+//                    result[i] = 0
+//                }
+//            }
+//        } else {
+//            for (i in nums.indices) {
+//                if (nums[i] == 0) {
+//                    result[i] = totalNonZeroProduct
+//                } else {
+//                    result[i] = totalNonZeroProduct / nums[i]
+//                }
+//            }
+//        }
+//
 
-        val totalNonZeroProduct = nums.filter { it != 0 }.fold(1) { acc, i -> acc * i }
+        val result = IntArray(nums.size)
+        val prefix = IntArray(nums.size)
+        val suffix = IntArray(nums.size)
 
-        if (countOfZero == 1) {
-            for (i in nums.indices) {
-                if (nums[i] == 0) {
-                    result[i] = totalNonZeroProduct
-                } else {
-                    result[i] = 0
-                }
-            }
-        } else {
-            for (i in nums.indices) {
-                if (nums[i] == 0) {
-                    result[i] = totalNonZeroProduct
-                } else {
-                    result[i] = totalNonZeroProduct / nums[i]
-                }
-            }
+        prefix[0] = 1
+        suffix[nums.size - 1] = 1
+
+        for (i in 1 until nums.size) {
+            prefix[i] = prefix[i-1] * nums[i-1]
         }
+
+        for (i in nums.size - 2 downTo 0) {
+            suffix[i] = suffix[i+1] * nums[i+1]
+        }
+
+        for (i in nums.indices) {
+            result[i] = prefix[i] * suffix[i]
+        }
+
         return result
     }
 }
