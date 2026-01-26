25e2dabacc945d36626730f945b5455dbd0ac835
humbledroid <carotkut12@gmail.com>
2026-01-26 16:18:56 +0530

    not so O(n)2 solution

--- src/main/kotlin/neetcode/ProductOfArrayExceptSelf.kt
+++ src/main/kotlin/neetcode/ProductOfArrayExceptSelf.kt
@@ -10,23 +10,48 @@ class ProductOfArrayExceptSelf {
 //                if(currentNum == 0) add(totalProduct) else add(totalProduct.div(currentNum).toInt())
 //            }
 //        }.toIntArray()
-        val result = IntArray(nums.size) { 0 }
-        for (i in nums.indices) {
-            var product = 1
-            for (j in nums.indices) {
-                if (i == j) {
-                    continue
-                }
-                product *= nums[j]
-            }
-            result[i] = product
-        }
+//        val result = IntArray(nums.size) { 0 }
+//        for (i in nums.indices) {
+//            var product = 1
+//            for (j in nums.indices) {
+//                if (i == j) {
+//                    continue
+//                }
+//                product *= nums[j]
+//            }
+//            result[i] = product
+//        }
+//
+//        return result
 
+        val countOfZero = nums.count { it == 0 }
+        val result = IntArray(nums.size) { 0 }
+        if (countOfZero > 1) return result
+
+        val totalNonZeroProduct = nums.filter { it != 0 }.fold(1) { acc, i -> acc * i }
+
+        if (countOfZero == 1) {
+            for (i in nums.indices) {
+                if (nums[i] == 0) {
+                    result[i] = totalNonZeroProduct
+                } else {
+                    result[i] = 0
+                }
+            }
+        } else {
+            for (i in nums.indices) {
+                if (nums[i] == 0) {
+                    result[i] = totalNonZeroProduct
+                } else {
+                    result[i] = totalNonZeroProduct / nums[i]
+                }
+            }
+        }
         return result
     }
 }
 
 fun main(args: Array<String>) {
     val productOfArrayExceptSelf = ProductOfArrayExceptSelf()
-    println(productOfArrayExceptSelf.productExceptSelf(intArrayOf(0,0)).joinToString(","))
+    println(productOfArrayExceptSelf.productExceptSelf(intArrayOf(-1, 0, 1, 2, 3)).joinToString(","))
 }
