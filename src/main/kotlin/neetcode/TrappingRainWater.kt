3c7b4e01f9bd045d9bde97de6c3887cda6f02188
humbledroid <carotkut12@gmail.com>
2026-02-02 22:50:35 +0530

    traps the rain water

---
+++ src/main/kotlin/neetcode/TrappingRainWater.kt
@@ -0,0 +1,30 @@
+package neetcode
+
+import kotlin.math.min
+
+class TrappingRainWater {
+    fun trap(height: IntArray): Int {
+        var result = 0
+
+        val maxLeft = IntArray(height.size) { 0 }
+        val maxRight = IntArray(height.size) { 0 }
+        for (i in 1 until height.size) {
+            maxLeft[i] = maxOf(height[i - 1], maxLeft[i-1])
+        }
+
+        for (i in height.size-2 downTo 0) {
+            maxRight[i] = maxOf(height[i + 1], maxRight[i+1])
+        }
+
+        for (i in height.indices) {
+            result += if((min(maxLeft[i], maxRight[i]) - height[i]) >= 0) min(maxLeft[i], maxRight[i]) - height[i] else 0
+        }
+
+        return result
+    }
+}
+
+fun main() {
+    val trapWater = TrappingRainWater()
+    println(trapWater.trap(intArrayOf(0,1,0,2,1,0,1,3,2,1,2,1)))
+}
