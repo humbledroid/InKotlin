d64a92c4146105c069116f7778753313d6a63ec6
humbledroid <carotkut12@gmail.com>
2026-02-03 21:13:47 +0530

    an optimized trader

--- src/main/kotlin/neetcode/MaxProfit.kt
+++ src/main/kotlin/neetcode/MaxProfit.kt
@@ -14,9 +14,20 @@ class MaxProfit {
         return maxProfit
     }
 
+    fun maxProfit2(prices: IntArray): Int {
+        var maxProfit = 0
+        var minBuy = Int.MAX_VALUE
+
+        for (i in prices) {
+            maxProfit = maxOf(maxProfit, i - minBuy)
+            minBuy = minOf(minBuy, i)
+        }
+        return maxProfit
+    }
+
 }
 
 fun main() {
     val maxProfit = MaxProfit()
-    println(maxProfit.maxProfit(intArrayOf(5, 1, 5, 6, 7, 1, 10)))
+    println(maxProfit.maxProfit2(intArrayOf(5, 1, 5, 6, 7, 1, 10)))
 }
