9d5eddf0594e513161a3e4ff65381441e2ca543e
humbledroid <carotkut12@gmail.com>
2026-02-08 23:18:58 +0530

    using a deque to be more efficient

--- src/main/kotlin/neetcode/MaxSlidingWindow.kt
+++ src/main/kotlin/neetcode/MaxSlidingWindow.kt
@@ -1,6 +1,7 @@
 package neetcode
 
 class MaxSlidingWindow {
+    // O(n)^2 doesnt work that great though
     fun maxSlidingWindow(nums: IntArray, k: Int): IntArray {
         val result = mutableListOf<Int>()
 
@@ -17,9 +18,38 @@ class MaxSlidingWindow {
 
         return result.toIntArray()
     }
+
+    // lets use deque to maintain maximum at the front of a
+    // queue for each window
+    fun maxSlidingWindowsDeque(nums: IntArray, k: Int): IntArray {
+        val result = mutableListOf<Int>()
+
+        val dequeue = ArrayDeque<Int>()
+        var left = 0
+
+        for(right in nums.indices) {
+            val currentNum = nums[right]
+            while(dequeue.isNotEmpty() && nums[dequeue.last()] < currentNum) {
+                dequeue.removeLast()
+            }
+
+            dequeue.addLast(right)
+
+            if(left > dequeue.first()){
+                dequeue.removeFirst()
+            }
+
+            if((right+1) >= k){
+                result.add(nums[dequeue.first()])
+                left+=1
+            }
+        }
+
+        return result.toIntArray()
+    }
 }
 
 fun main() {
     val maxSlidingWindow = MaxSlidingWindow()
-    println(maxSlidingWindow.maxSlidingWindow(intArrayOf(1,2,1,0,4,2,6), k = 3))
+    println(maxSlidingWindow.maxSlidingWindowsDeque(intArrayOf(1,2,1,0,4,2,6), k = 3).joinToString(","))
 }
