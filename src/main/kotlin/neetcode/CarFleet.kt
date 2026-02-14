0eff999c0f1a5e2bbe1d87dc117a6b1767ed4538
humbledroid <carotkut12@gmail.com>
2026-02-14 15:55:27 +0530

    the stacK way

--- src/main/kotlin/neetcode/CarFleet.kt
+++ src/main/kotlin/neetcode/CarFleet.kt
@@ -1,25 +1,45 @@
 package neetcode
 
 class CarFleet {
+//    fun carFleet(target: Int, position: IntArray, speed: IntArray): Int {
+//        val remainingTimeArray = DoubleArray(target) { 0.toDouble() }
+//        var fleetCount = 0
+//
+//        for (i in position.indices) {
+//            val currentCar = position[i]
+//            remainingTimeArray[currentCar] = (target - currentCar) / speed[i].toDouble()
+//        }
+//
+//        var lastFleetCarTime = 0.toDouble()
+//        for (i in remainingTimeArray.size - 1 downTo 0) {
+//            val currenCarTime = remainingTimeArray[i]
+//            if (currenCarTime > lastFleetCarTime) {
+//                lastFleetCarTime = currenCarTime
+//                fleetCount++
+//            }
+//        }
+//
+//        return fleetCount
+//    }
+
     fun carFleet(target: Int, position: IntArray, speed: IntArray): Int {
-        val remainingTimeArray = DoubleArray(target) { 0.toDouble() }
-        var fleetCount = 0
 
-        for (i in position.indices) {
-            val currentCar = position[i]
-            remainingTimeArray[currentCar] = (target - currentCar) / speed[i].toDouble()
-        }
+        val pair = position.zip(speed).sortedByDescending { it.first }
+        println(pair)
 
-        var lastFleetCarTime = 0.toDouble()
-        for (i in remainingTimeArray.size - 1 downTo 0) {
-            val currenCarTime = remainingTimeArray[i]
-            if (currenCarTime > lastFleetCarTime) {
-                lastFleetCarTime = currenCarTime
-                fleetCount++
+        val stack = mutableListOf<Double>()
+
+        for((p, s) in pair) {
+            val remainingTime = (target - p)/s.toDouble()
+            stack.add(remainingTime)
+            if(stack.size >= 2 && stack[stack.size - 1] <= stack[stack.size - 2]) {
+                stack.removeAt(stack.size - 1)
             }
+
         }
 
-        return fleetCount
+        return stack.size
+
     }
 }
 
