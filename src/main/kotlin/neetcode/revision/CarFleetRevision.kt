6476101a9364fa4d6f63c7e16a6749a35f0027fa
humbledroid <carotkut12@gmail.com>
2026-02-15 22:45:39 +0530

    without stack and the usual sort

--- src/main/kotlin/neetcode/revision/CarFleetRevision.kt
+++ src/main/kotlin/neetcode/revision/CarFleetRevision.kt
@@ -8,7 +8,7 @@ class CarFleetRevision {
 
         val pairSortedByDistanceTravelled = position.zip(speed).sortedByDescending { it.first }
 
-        for((p, s) in pairSortedByDistanceTravelled){
+        for ((p, s) in pairSortedByDistanceTravelled) {
             val remainingTimeToTarget = (target - p) / s.toDouble()
             stack.push(remainingTimeToTarget)
             if (stack.size >= 2 && stack[stack.size - 1] <= stack[stack.size - 2]) {
@@ -17,9 +17,29 @@ class CarFleetRevision {
         }
         return stack.size
     }
+
+    fun carFleetWithoutStack(target: Int, position: IntArray, speed: IntArray): Int {
+        var fleetCount = 0
+        val remainingTimeForEachCar = DoubleArray(target) { 0.toDouble() }
+
+        for(i in position.indices) {
+            val currentCar = position[i]
+            remainingTimeForEachCar[currentCar] = (target - currentCar) / speed[i].toDouble()
+        }
+
+        var lastFleetCarTime = 0.toDouble()
+        for(i in remainingTimeForEachCar.size - 1 downTo 0) {
+            if(remainingTimeForEachCar[i] > lastFleetCarTime) {
+                lastFleetCarTime = remainingTimeForEachCar[i]
+                fleetCount++
+            }
+        }
+
+        return fleetCount
+    }
 }
 
 fun main() {
     val carFleetRevision = CarFleetRevision()
-    println(carFleetRevision.carFleet(target = 10, position = intArrayOf(4,1,0,7), speed = intArrayOf(2,2,1,1)))
+    println(carFleetRevision.carFleetWithoutStack(target = 10, position = intArrayOf(4, 1, 0, 7), speed = intArrayOf(2, 2, 1, 1)))
 }
