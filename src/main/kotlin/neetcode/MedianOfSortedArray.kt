948e64c7ac82a68cab27a5e3af58e7ce91fc94d3
humbledroid <carotkut12@gmail.com>
2026-02-21 22:45:29 +0530

    some more kotlin magic i guess

--- src/main/kotlin/neetcode/MedianOfSortedArray.kt
+++ src/main/kotlin/neetcode/MedianOfSortedArray.kt
@@ -5,42 +5,30 @@ import kotlin.math.min
 
 class MedianOfSortedArray {
     fun findMedianSortedArrays(nums1: IntArray, nums2: IntArray): Double {
-        var a = nums1
-        var b = nums2
-        val total = a.size + b.size
-        val half = (total + 1) / 2
-
-        if (b.size < a.size) {
-            a = nums2
-            b = nums1
-        }
-        val totalSize = a.size + b.size
+        val (a, b) = if (nums1.size <= nums2.size) nums1 to nums2 else nums2 to nums1
+        val half = (a.size + b.size + 1) / 2
+        val isEven = (a.size + b.size) % 2 == 0
 
         var left = 0
         var right = a.size
 
-        while(left <= right){
-            val aMid  = (left + right) / 2 // Mid of A
+        while (left <= right) {
+            val aMid = (left + right) / 2
             val bMid = half - aMid
 
-            val aLeft = if(aMid > 0) a[aMid - 1] else Int.MIN_VALUE
-            val aRight = if(aMid < a.size) a[aMid] else Int.MAX_VALUE
-            val bLeft = if(bMid > 0 ) b[bMid - 1] else Int.MIN_VALUE
-            val bRight = if(bMid < b.size) b[bMid] else Int.MAX_VALUE
+            val aLeft  = a.getOrElse(aMid - 1) { Int.MIN_VALUE }
+            val aRight = a.getOrElse(aMid)     { Int.MAX_VALUE }
+            val bLeft  = b.getOrElse(bMid - 1) { Int.MIN_VALUE }
+            val bRight = b.getOrElse(bMid)     { Int.MAX_VALUE }
 
-            // correct partition here
-            if(aLeft <= bRight && bLeft <= aRight) {
-                if(totalSize % 2 == 0){
-                    return (max(aLeft, bLeft) + min(aRight, bRight))/2.toDouble()
-                }else{
-                    return max(aLeft, bLeft).toDouble()
-                }
-            }else if(aLeft > bRight) {
-                right = aMid - 1
-            } else {
-                left = aMid + 1
+            when {
+                aLeft <= bRight && bLeft <= aRight -> return if (isEven)
+                    (maxOf(aLeft, bLeft) + minOf(aRight, bRight)) / 2.0
+                else
+                    maxOf(aLeft, bLeft).toDouble()
+                aLeft > bRight -> right = aMid - 1
+                else           -> left  = aMid + 1
             }
-
         }
         return 0.0
     }
