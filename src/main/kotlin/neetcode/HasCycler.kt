91604bcb3a652488b671f566e1c32b1d505e50da
humbledroid <carotkut12@gmail.com>
2026-02-23 23:36:09 +0530

    lets see some cycles

---
+++ src/main/kotlin/neetcode/HasCycler.kt
@@ -0,0 +1,19 @@
+package neetcode
+
+class HasCycle {
+    fun hasCycle(head: ListNode?): Boolean {
+        var slow = head
+        var fast = head
+
+        while(fast?.next != null) {
+            slow = slow?.next
+            fast = fast.next?.next
+
+            if(slow == fast) {
+                return true
+            }
+        }
+
+        return false
+    }
+}
