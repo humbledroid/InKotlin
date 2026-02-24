35071af38178fd9ea69c37cbe2b96ccd93b93ff1
humbledroid <carotkut12@gmail.com>
2026-02-24 21:06:57 +0530

    merge and replace

--- src/main/kotlin/neetcode/ReOrderList.kt
+++ src/main/kotlin/neetcode/ReOrderList.kt
@@ -26,5 +26,46 @@ class ReOrderList {
 
         nodes[i].next = null
     }
+
+    fun reorderListReverseMerge(head: ListNode?): Unit {
+        if(head?.next == null) return
+
+        var slow: ListNode? = head
+        var fast: ListNode? = head.next
+
+        while (fast?.next != null) {
+            slow = slow?.next
+            fast = fast.next?.next
+        }
+        /**
+         * by this time, slow will be at the half and fast will be at the end
+         */
+
+        val second = slow?.next
+        slow?.next = null
+
+        var previous: ListNode? = null
+
+        var currentNode = second
+
+        while(currentNode != null) {
+            val tempNode = currentNode.next
+            currentNode.next = previous
+            previous = currentNode
+            currentNode = tempNode
+        }
+
+        var first: ListNode? = head
+        var secondList: ListNode? = previous
+
+        while(first != null && secondList != null) {
+            val tempNode = first.next
+            val tempNode2 = secondList.next
+            first.next = secondList
+            secondList.next = tempNode
+            first = tempNode
+            secondList = tempNode2
+        }
+    }
 }
 
