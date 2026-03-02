8485fceeeb3c8554d5400eb2f17fc5e8c8cbbb4b
humbledroid <carotkut12@gmail.com>
2026-03-02 20:25:52 +0530

    invert a tree easy

---
+++ src/main/kotlin/neetcode/tree/InvertTree.kt
@@ -0,0 +1,28 @@
+package neetcode.tree
+
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
+
+class InvertTree {
+    fun invertTree(root: TreeNode?): TreeNode? {
+        if (root == null) return null
+
+        val queue: ArrayDeque<TreeNode?> = ArrayDeque()
+        queue.add(root)
+
+        while (queue.isNotEmpty()) {
+            val node = queue.removeFirst()
+            node?.let {
+                val temp = it.left
+                it.left = it.right
+                it.right = temp
+                queue.add(it.left)
+                queue.add(it.right)
+            }
+        }
+
+        return root
+    }
+}
