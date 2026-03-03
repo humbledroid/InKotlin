bc468ba8f010b12274cd6eb02f14ee7a5d3b8a0b
humbledroid <carotkut12@gmail.com>
2026-03-03 21:40:12 +0530

    some revision on invert

---
+++ src/main/kotlin/neetcode/revision/tree/InvertTreeRev.kt
@@ -0,0 +1,34 @@
+package neetcode.revision.tree
+
+import neetcode.tree.TreeNode
+
+class InvertTreeRev {
+    fun invertTree(root: TreeNode?): TreeNode? {
+        if (root?.left == null && root?.right == null) return root
+
+        val deque = ArrayDeque<TreeNode>()
+        deque.add(root)
+        while (deque.isNotEmpty()) {
+            val currentNode = deque.first()
+
+            if (currentNode.left != null) deque.add(currentNode.left!!)
+            if (currentNode.right != null) deque.add(currentNode.right!!)
+
+            val tmp = currentNode.left
+            currentNode.left = currentNode.right
+            currentNode.right = tmp
+
+            deque.removeFirst()
+        }
+
+        return root
+    }
+
+    fun invertTreeRec(root: TreeNode?): TreeNode? {
+        if (root == null) return null
+        return root.apply { left = invertTreeRec(right).also { right = invertTreeRec(root.left) } }
+    }
+
+
+
+}
