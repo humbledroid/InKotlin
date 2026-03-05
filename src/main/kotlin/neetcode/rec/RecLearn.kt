5be3797a086891e80e954de31dc86b544255b5c6
humbledroid <carotkut12@gmail.com>
2026-03-05 22:37:18 +0530

    add visualization of call frame

--- src/main/kotlin/neetcode/rec/RecLearn.kt
+++ src/main/kotlin/neetcode/rec/RecLearn.kt
@@ -11,6 +11,25 @@ fun main() {
     }
 
     countDown(4)
+
+    fun sum(input: List<Int>): Int {
+        if(input.isEmpty()) return 0 // Base case
+        return input.first() + sum(input.drop(1))
+                               // this is the smaller sub problem that needs to be solved
+    }
+
+    println(sum(listOf(3,7,2)))
+
+    /**
+     * sum([3, 7, 2])
+     *   → 3 + sum([7, 2])
+     *     → 7 + sum([2])
+     *       → 2 + sum([])
+     *         → 0   // base case!
+     *       ← 2 + 0 = 2
+     *     ← 7 + 2 = 9
+     *   ← 3 + 9 = 12
+     */
 }
 
 
