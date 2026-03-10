33046bdfe85e461cb6350e7eaa71ec1fa65348cb
humbledroid <carotkut12@gmail.com>
2026-03-10 23:26:15 +0530

    revision of right side view:

---
+++ src/main/kotlin/neetcode/revision/tree/RightSideViewBinaryTreeRev.kt
@@ -0,0 +1,40 @@
+package neetcode.revision.tree
+
+import neetcode.tree.MaxDepth
+import neetcode.tree.RightSideViewBinaryTree
+import neetcode.tree.TreeNode
+
+class RightSideViewBinaryTreeRev {
+    fun rightSideView(root: TreeNode?): List<Int> {
+        val result = mutableListOf<Int>()
+
+        fun dfs(root: TreeNode?, depth: Int) {
+            if (root == null) return
+            if(depth == result.size) {
+                result.add(root.`val`)
+            }
+
+            dfs(root.right, depth + 1)
+            dfs(root.left, depth + 1)
+        }
+
+        dfs(root, 0)
+
+        return  result
+    }
+}
+
+fun main() {
+    val rightSideViewBinaryTree = RightSideViewBinaryTreeRev()
+    val root = TreeNode(1).apply {
+        left = TreeNode(2).apply {
+            left = TreeNode(4)
+            right = TreeNode(5)
+        }
+        right = TreeNode(3).apply {
+            left = TreeNode(6)
+            right = TreeNode(7)
+        }
+    }
+    println(rightSideViewBinaryTree.rightSideView(root))
+}
