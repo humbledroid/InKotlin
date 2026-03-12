f5bd8458c36e10a7a308cd24276ffc72d682f12a
humbledroid <carotkut12@gmail.com>
2026-03-12 23:11:08 +0530

    revision of valid bst

--- src/main/kotlin/neetcode/tree/ValidBST.kt
+++ src/main/kotlin/neetcode/tree/ValidBST.kt
@@ -12,3 +12,17 @@ class ValidBST {
         return dfs(root, Long.MIN_VALUE, Long.MAX_VALUE)
     }
 }
+
+class ValidBSTRev {
+    fun isValidBST(root: TreeNode?): Boolean {
+
+        fun dfs(node: TreeNode?, min: Long, max: Long): Boolean {
+            if (node == null) return true
+            if(node.`val` !in (min+1)..< max)return false
+
+            return dfs(node.left, min, node.`val`.toLong()) && dfs(node.right, node.`val`.toLong(), max)
+        }
+
+        return dfs(root, Long.MIN_VALUE, Long.MAX_VALUE)
+    }
+}
