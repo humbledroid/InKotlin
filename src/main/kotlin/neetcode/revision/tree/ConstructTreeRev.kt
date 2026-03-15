78f4c78deacfb401d24233225db1345ef3465bb8
humbledroid <carotkut12@gmail.com>
2026-03-15 21:30:01 +0530

    revision of construct a tree

---
+++ src/main/kotlin/neetcode/revision/tree/ConstructTreeRev.kt
@@ -0,0 +1,26 @@
+package neetcode.revision.tree
+
+import neetcode.tree.TreeNode
+
+class ConstructTreeRev {
+    private var preOrderIndex = 0
+    private var inOrderIndex = 0
+    fun buildTree(preorder: IntArray, inorder: IntArray): TreeNode? {
+        fun dfs(limit: Int): TreeNode? {
+            if(preOrderIndex >= preorder.size) return null
+            if(inorder[inOrderIndex] == limit) {
+                inOrderIndex++
+                return null
+            }
+
+            val root = TreeNode(preorder[preOrderIndex])
+            preOrderIndex++
+
+            root.left = dfs(root.`val`)
+            root.right = dfs(limit)
+
+            return root
+        }
+        return dfs(Int.MAX_VALUE)
+    }
+}
