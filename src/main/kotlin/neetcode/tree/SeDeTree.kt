32bf4cc8c5288b58243ab52671bcc9777dd0dcfd
humbledroid <carotkut12@gmail.com>
2026-03-16 23:51:10 +0530

    serialize deserialize tree

---
+++ src/main/kotlin/neetcode/tree/SeDeTree.kt
@@ -0,0 +1,45 @@
+package neetcode.tree
+
+class SeDeTree {
+    // Encodes a tree to a single string.
+    fun serialize(root: TreeNode?): String {
+        val result = mutableListOf<String>()
+
+        fun dfs(node: TreeNode?) {
+            if(node == null) {
+                result.add("N")
+                return
+            }
+
+            result.add(node.`val`.toString())
+            dfs(node.left)
+            dfs(node.right)
+        }
+
+        dfs(root)
+        return result.joinToString(",")
+    }
+
+    // Decodes your encoded data to tree.
+    fun deserialize(data: String): TreeNode? {
+        val splitData = data.split(",")
+        var i = 0
+
+        fun dfs(): TreeNode? {
+            if(splitData[i] == "N"){
+                i++
+                return null
+            }
+
+            val node = TreeNode(splitData[i].toInt())
+            i++
+            node.left = dfs()
+            node.right = dfs()
+
+            return node
+        }
+
+
+        return dfs()
+    }
+}
