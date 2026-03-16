0e0044e106d615efd0f5eaefe58356fa2dfdc827
humbledroid <carotkut12@gmail.com>
2026-03-16 23:42:24 +0530

    revision of max  path sum

---
+++ src/main/kotlin/neetcode/revision/tree/MaxPathSumRev.kt
@@ -0,0 +1,25 @@
+package neetcode.revision.tree
+
+import neetcode.tree.TreeNode
+
+class MaxPathSumRev {
+
+    private var result = Int.MIN_VALUE
+
+    fun maxPathSum(root: TreeNode?): Int {
+        if(root == null) return 0
+        dfs(root)
+        return result
+    }
+
+    private fun dfs(node: TreeNode?): Int {
+        if(node == null) return 0
+
+        val leftMax = maxOf(dfs(node.left), 0)
+        val rightMax = maxOf(dfs(node.right), 0)
+
+        result = maxOf(result, node.`val` + leftMax + rightMax)
+        return node.`val` + maxOf(leftMax, rightMax)
+    }
+
+}
