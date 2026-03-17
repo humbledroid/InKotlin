36a672c5633201246c0519cc2340628adb3e7ff8
humbledroid <carotkut12@gmail.com>
2026-03-17 23:25:03 +0530

    the correct solution using PQ

--- src/main/kotlin/neetcode/heap/KthLargest.kt
+++ src/main/kotlin/neetcode/heap/KthLargest.kt
@@ -1,11 +1,34 @@
 package neetcode.heap
 
+import java.util.PriorityQueue
+
 class KthLargest(val k: Int, nums: IntArray) {
-    private val input = nums.toMutableList()
-    
-    fun add(`val`: Int): Int {
-        input.add(`val`)
-        input.sort()
-        return input[input.size - k]
+//    private val input = nums.toMutableList()
+//
+//    fun add(`val`: Int): Int {
+//        input.add(`val`)
+//        input.sort()
+//        return input[input.size - k]
+//    }
+
+    val minHeap = PriorityQueue<Int>()
+
+    init {
+        nums.forEach {
+            minHeap.offer(it)
+        }
+
+        while(minHeap.size > k) {
+            minHeap.poll()
+        }
+    }
+
+    fun add(`val`: Int) : Int{
+        minHeap.offer(`val`)
+        if(minHeap.size > k){
+            minHeap.poll()
+        }
+
+        return minHeap.peek()
     }
 }
