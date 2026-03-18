a8aa7dfc8c9c4b10e25b7e686b26764589acf10e
humbledroid <carotkut12@gmail.com>
2026-03-18 22:57:35 +0530

    last stone weight using minHeap with negative weights stored

---
+++ src/main/kotlin/neetcode/heap/LastStoneWeight.kt
@@ -0,0 +1,24 @@
+package neetcode.heap
+
+import java.util.PriorityQueue
+
+class LastStoneWeight {
+    fun lastStoneWeight(stones: IntArray): Int {
+        val minHeap = PriorityQueue<Int>()
+
+        stones.forEach {
+            minHeap.offer(-it)
+        }
+
+        while(minHeap.size > 1){
+            val first = minHeap.poll()
+            val second = minHeap.poll()
+
+            if(second > first){
+                minHeap.offer(first-second)
+            }
+        }
+        minHeap.offer(0)
+        return Math.abs(minHeap.peek())
+    }
+}
