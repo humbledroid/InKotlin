03052bfa9ebfc3b8e7643747c639eb792751b25b
humbledroid <carotkut12@gmail.com>
2026-03-19 23:14:12 +0530

    k closes points on coordinate system

---
+++ src/main/kotlin/neetcode/heap/KClosesPoints.kt
@@ -0,0 +1,20 @@
+package neetcode.heap
+
+import java.util.PriorityQueue
+
+class KClosesPoints {
+    fun kClosest(points: Array<IntArray>, k: Int): Array<IntArray> {
+        val minHeap = PriorityQueue(compareBy<IntArray> { it[0] * it[0] + it[1] * it[1] })
+
+        points.forEach {
+            minHeap.offer(it)
+        }
+
+        val res = Array<IntArray>(k){ IntArray(2) }
+        repeat(k) {
+            res[it] = minHeap.poll()
+        }
+
+        return res
+    }
+}
