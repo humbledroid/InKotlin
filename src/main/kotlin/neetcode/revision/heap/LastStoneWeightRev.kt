c5e57d77490d0b3c9dd440fd1c21b476dae557e4
humbledroid <carotkut12@gmail.com>
2026-03-19 23:08:03 +0530

    revision of last stone weight

---
+++ src/main/kotlin/neetcode/revision/heap/LastStoneWeightRev.kt
@@ -0,0 +1,25 @@
+package neetcode.revision.heap
+
+import java.util.PriorityQueue
+import kotlin.math.abs
+
+class LastStoneWeightRev {
+    fun lastStoneWeight(stones: IntArray): Int {
+        val minHeap = PriorityQueue<Int>()
+        stones.forEach {
+            minHeap.offer(-it)
+        }
+
+        while(minHeap.size > 1){
+            val first = minHeap.poll()
+            val second = minHeap.poll()
+
+            if(second > first){
+                minHeap.offer(first -  second)
+            }
+        }
+        minHeap.offer(0)
+        return abs(minHeap.peek())
+
+    }
+}
