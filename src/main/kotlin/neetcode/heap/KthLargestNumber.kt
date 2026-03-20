fb22177219849b451c0cdf247fd2a348979fe89f
humbledroid <carotkut12@gmail.com>
2026-03-21 00:21:28 +0530

    the heap way as we did yesterday

--- src/main/kotlin/neetcode/heap/KthLargestNumber.kt
+++ src/main/kotlin/neetcode/heap/KthLargestNumber.kt
@@ -1,9 +1,22 @@
 package neetcode.heap
 
+import java.util.PriorityQueue
+
 class KthLargestNumber {
     // the sorting way :face_palm:
     fun findKthLargest(nums: IntArray, k: Int): Int {
-        nums.sort()
-        return nums[nums.size - k]
+//        nums.sort()
+//        return nums[nums.size - k]
+//    }
+
+        val minHeap = PriorityQueue<Int>()
+
+        nums.forEach { num ->
+            minHeap.offer(num)
+            if (minHeap.size > k) {
+                minHeap.poll()
+            }
+        }
+        return minHeap.peek()
     }
 }
