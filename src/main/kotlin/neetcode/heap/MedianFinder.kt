878b0ed7ca4c43582be962c5cee1b32485684543
humbledroid <carotkut12@gmail.com>
2026-03-23 23:43:33 +0530

    median without sort

--- src/main/kotlin/neetcode/heap/MedianFinder.kt
+++ src/main/kotlin/neetcode/heap/MedianFinder.kt
@@ -1,19 +1,47 @@
 package neetcode.heap
 
 class MedianFinder {
-    private val data = mutableListOf<Int>()
+//    private val data = mutableListOf<Int>()
+//
+//    fun addNum(num: Int) {
+//        data.add(num)
+//    }
+//
+//    fun findMedian(): Double {
+//        data.sort()
+//        val n = data.size
+//        return if (n % 2 == 1) {
+//            data[n / 2].toDouble()
+//        } else {
+//            (data[n / 2] + data[n / 2 - 1]) / 2.0
+//        }
+//    }
 
-    fun addNum(num: Int) {
-        data.add(num)
+
+    private val small = PriorityQueue<Int>(compareByDescending { it }) // max heap
+    private val large = PriorityQueue<Int>() // min heap
+
+    fun addNum(num: Int){
+        if (large.isNotEmpty() && num > large.peek()){
+            large.add(num)
+        } else {
+            small.add(num)
+        }
+
+        if(small.size > large.size + 1) {
+            large.add(small.poll())
+        }
+
+        if(large.size > small.size + 1) {
+            small.add(large.poll())
+        }
     }
 
     fun findMedian(): Double {
-        data.sort()
-        val n = data.size
-        return if (n % 2 == 1) {
-            data[n / 2].toDouble()
-        } else {
-            (data[n / 2] + data[n / 2 - 1]) / 2.0
+        return when {
+            small.size > large.size -> small.peek().toDouble()
+            large.size > small.size -> large.peek().toDouble()
+            else -> (small.peek() + large.peek()) / 2.0
         }
     }
 }
