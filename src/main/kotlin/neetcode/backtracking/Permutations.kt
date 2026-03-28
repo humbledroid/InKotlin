c141cbe32acd191ba571b94cf88ef3b841ab7c80
humbledroid <carotkut12@gmail.com>
2026-03-28 22:48:25 +0530

    optimal permutations using backtracking

--- src/main/kotlin/neetcode/backtracking/Permutations.kt
+++ src/main/kotlin/neetcode/backtracking/Permutations.kt
@@ -1,18 +1,47 @@
 package neetcode.backtracking
 
-class Permutations {
-    fun permute(nums: IntArray): List<List<Int>> {
-        if (nums.isEmpty()) return listOf(listOf())
+/**
+ * See fig [permutations.png]
+ */
 
-        val perms = permute(nums.sliceArray(1 until nums.size))
-        val res = mutableListOf<List<Int>>()
-        for (p in perms) {
-            for (i in 0..p.size) {
-                val pCopy = p.toMutableList()
-                pCopy.add(i, nums[0])
-                res.add(pCopy)
-            }
-        }
+class Permutations {
+//    fun permute(nums: IntArray): List<List<Int>> {
+//        if (nums.isEmpty()) return listOf(listOf())
+//
+//        val perms = permute(nums.sliceArray(1 until nums.size))
+//        val res = mutableListOf<List<Int>>()
+//        for (p in perms) {
+//            for (i in 0..p.size) {
+//                val pCopy = p.toMutableList()
+//                pCopy.add(i, nums[0])
+//                res.add(pCopy)
+//            }
+//        }
+//        return res
+//    }
+
+    private val res = mutableListOf<List<Int>>()
+
+    fun permute(nums: IntArray): List<List<Int>> {
+        backtrack(nums, 0)
         return res
+    }
+
+    private fun backtrack(nums: IntArray, idx: Int) {
+        if (idx == nums.size) {
+            res.add(nums.toList())
+            return
+        }
+        for (i in idx until nums.size) {
+            nums.swap(idx, i)
+            backtrack(nums, idx + 1)
+            nums.swap(idx, i)
+        }
+    }
+
+    private fun IntArray.swap(i: Int, j: Int) {
+        val temp = this[i]
+        this[i] = this[j]
+        this[j] = temp
     }
 }
