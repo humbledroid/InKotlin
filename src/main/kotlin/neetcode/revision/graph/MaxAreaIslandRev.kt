b87771dddf60837c093792e9a818193a8f67b19c
humbledroid <carotkut12@gmail.com>
2026-04-08 23:40:11 +0530

    some optimizations

--- src/main/kotlin/neetcode/revision/graph/MaxAreaIslandRev.kt
+++ src/main/kotlin/neetcode/revision/graph/MaxAreaIslandRev.kt
@@ -4,21 +4,20 @@ class MaxAreaIslandRev {
     fun maxAreaOfIsland(grid: Array<IntArray>): Int {
         val rows = grid.size
         val cols = grid[0].size
-        val visit = HashSet<Pair<Int, Int>>()
 
         fun dfs(r: Int, c: Int): Int {
-            if (r < 0 || r >= rows || c < 0 || c >= cols ||
-                grid[r][c] == 0 || visit.contains(r to c)) {
-                return 0
-            }
-            visit.add(r to c)
+            if (r !in 0..<rows || c < 0 || c >= cols || grid[r][c] == 0) return 0
+            grid[r][c] = 0
             return 1 + dfs(r + 1, c) + dfs(r - 1, c) + dfs(r, c + 1) + dfs(r, c - 1)
         }
 
         var area = 0
         for (r in 0 until rows) {
             for (c in 0 until cols) {
-                area = maxOf(area, dfs(r, c))
+                if (grid[r][c] == 1) {
+                    val a = dfs(r, c)
+                    if (a > area) area = a
+                }
             }
         }
         return area
